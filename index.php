f6f26a242cedcffb18cb0b5dcb6e6027c577b888
laravelgr8 <82206712+laravelgr8@users.noreply.github.com>
2022-08-03 09:27:29 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+git config --global user.name "Suman"
+git config --global user.email "suman@gmail.com"
+git init
+git add .
+git commit -m "Comment Here"
+git remote add origin ssh code paste here
+ssh-keygen -t rsa -b 4096 -C "suman@gmail.com"
+Public Key path copy kare
+cat public key paste here
+ssh code copy and go to github setting ssk key
+git push origin master
+-------------------------------------------
+How to clone Github repository
+
+git clone https://github.com/laravelgr8/DataTableNotes.git
+or
+git -c http.sslVerify=false clone https://github.com/laravelgr8/DataTableNotes.git
+
+--------------------------------------------
+How to get current directory
+pwd
+--------------------------------------------
+How to get all file name
+La
+--------------------------------------------
